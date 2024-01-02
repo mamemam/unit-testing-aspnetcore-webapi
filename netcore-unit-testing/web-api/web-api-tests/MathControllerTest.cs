e6e9beacf37d9cd3d340a91e55ad148ec0bc4fac
mamemam <mam_emam_14@yahoo.com>
2024-01-02 21:09:01 +0300

    Add Math web api with thier test

---
+++ netcore-unit-testing/web-api/web-api-tests/MathControllerTest.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using web_api.Contracts;
+using web_api.Controllers;
+using web_api.Model;
+using Xunit;
+
+namespace web_api_tests
+{
+    public class MathControllerTests
+    {
+        [Fact]
+        public void Add_ReturnsCorrectResult()
+        {
+            // Arrange
+            var controller = new MathController();
+
+            // Act
+            var result = controller.Add(5, 3) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(8, (double)result.Value);
+        }
+
+        [Fact]
+        public void Subtract_ReturnsCorrectResult()
+        {
+            // Arrange
+            var controller = new MathController();
+
+            // Act
+            var result = controller.Subtract(8, 3) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(5, (double)result.Value);
+        }
+
+        [Fact]
+        public void Multiply_ReturnsCorrectResult()
+        {
+            // Arrange
+            var controller = new MathController();
+
+            // Act
+            var result = controller.Multiply(4, 6) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(24, (double)result.Value);
+        }
+
+        [Fact]
+        public void Divide_ReturnsCorrectResult()
+        {
+            // Arrange
+            var controller = new MathController();
+
+            // Act
+            var result = controller.Divide(10, 2) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(5, (double)result.Value);
+        }
+
+        [Fact]
+        public void Divide_ByZero_ReturnsBadRequest()
+        {
+            // Arrange
+            var controller = new MathController();
+
+            // Act
+            var result = controller.Divide(10, 0) as BadRequestObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Division by zero is not allowed", result.Value);
+        }
+    }
+}
---
+++ netcore-unit-testing/web-api/web-api/Controllers/MathController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpGet("add")]
+        public IActionResult Add(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return Ok(result);
+        }
+
+        [HttpGet("subtract")]
+        public IActionResult Subtract(double num1, double num2)
+        {
+            double result = num1 - num2;
+            return Ok(result);
+        }
+
+        [HttpGet("multiply")]
+        public IActionResult Multiply(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return Ok(result);
+        }
+
+        [HttpGet("divide")]
+        public IActionResult Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                return BadRequest("Division by zero is not allowed");
+            }
+
+            double result = num1 / num2;
+            return Ok(result);
+        }
+    }
+}
